0c3f836ed782f9a11725dc5c35ed8631c6d572b6
zenan <lotungsm@gmail.com>
2015-01-15 03:07:59 -0800

    Update research.php

--- pages/research.php
+++ pages/research.php
@@ -4,8 +4,20 @@ $template->title = 'Research';
 $template->tab = 'Research';
 ?>
 
-<h2>Research Projects and Sponsors</h2>
+<h2>Building Synthetic Biological Circuits for Medical Applications</h2>
 
-<p>Coming Soon!</p>
+<p>Synthetic biology—an emerging field that applies foundational technologies including high-throughput DNA synthesis, sequencing, and assembly to the construction of novel biological systems with programmed functionality—has generated numerous model systems capable of increasingly complex functions in recent years. 
+</p>
+
+<p>Our laboratory is interested in applying synthetic biology expertise to the engineering of novel biological circuits with direct applications in health and medicine. Our focus is on mammalian systems, particularly in cell-based therapy for cancer. Current projects include rewiring cytokine-signaling pathways in T cells that can recognize the tumor microenvironment and, in response to tumor detection, simultaneously execute tumor-killing functions and recruit supporting immune responders to the tumor site. These novel systems aim to enhance the safety and efficacy of cell-based therapy to tackle currently incurable diseases.
+</p>
+
+<h2>Engineering Next-Generation Chimeric Antigen Receptors</h2>
+
+<p>Chimeric antigen receptors (CARs) are synthetic, fusion proteins composed of antibody-based extracellular domains linked to intracellular signaling domains typically derived from the natural T-cell receptor complex (e.g., CD3 zeta chain). CAR-modified T cells have shown exciting potential in treating otherwise intractable diseases such as relapsing leukemia and lymphoma, metastatic melanoma, and glioblastoma multiforme. 
+</p>
+
+<p>State-of-the-art CARs are single-input, single-output devices that execute a killing function in response to the detection of one antigen signal. As a result, CAR-based therapies are vulnerable to off-target toxicity toward healthy cells as well as mutational escape by tumor cells. Our laboratory is developing next-generation CARs that can perform logical computation of multiple input signals, thereby increasing the specificity as well as versatility of CAR-modified T cells for cancer therapy.
+</p>
 
 </script>
