c8a61a32a2c23383b50fc505a1b2a8ea0683d137
zenan <lotungsm@gmail.com>
2015-01-18 01:40:03 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-date_default_timezone_set('America/New_York');
+date_default_timezone_set('America/Los_Angeles');
 
 require_once('includes/template.php');
 require_once('includes/publications.php');
